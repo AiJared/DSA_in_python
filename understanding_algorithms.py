a1af990b9f67015da126b4a5a77513b1d1539356
Kerriea-star <ojared511@gmail.com>
2024-05-19 12:00:33 +0300

    evaluating an algorithm

--- understanding_algorithms.py
+++ understanding_algorithms.py
@@ -43,3 +43,73 @@ item we are searching for or run out of items.
 4. The algorithm should produce a result.
 5. The algorithm should complete in a finit amount of time.
 """
+
+# Evaluating an algorithm
+"""
+An algorithm is evaluated through:
+1. Correctness
+2. Efficiency.
+"""
+
+# 1. Correctness
+"""
+An algorithm is deemed as correct if everytime we run it against all possible input
+data we get an output we expect.
+
+Part of correctness also means that for every possible input the algorithm should
+always terminate/end.
+
+If the above two are not met, then the algorithm is not correct.
+"""
+
+# 2. Efficiency
+"""
+Efficiency helps us solve a problem faster.
+
+There are two measures when it comes to evaluating efficiency
+
+1. Time and
+2. Space.
+
+Time complexity is the measure of how long it takes for a program
+to run.
+
+Space complexity - deals with the amount of memory taken up on the computer
+
+Good algorithms need to measure between these two to be useful.
+For example, if an algorithm is extremely fast but consumes more memoru than
+you have available then it is not a good algorithm.
+
+"""
+
+# Metric of measuring Time and Space Complexities
+"""
+1. Running Time
+
+The measure of the amount of time it took for an algorithm to run for a given set
+of values. It is used to define time compexity.
+
+The are three ways we can measure how well an algorithm does
+
+a. Best Case Scenario - Happens when the algorithm takes less running time to complete the task 
+b. Average Case Scenario - Is the average running time an algorithm takes to complete the task.
+c. Worst Case Scenario - This is the maximum amount of time an algorithm can take to complete a task.
+
+While going back to the real life scenario of thinking of a number and letting other people guess 
+the correct number, we have already seen how Linear Search can be used to achieve, now let's see how
+how another search algorithm can be used to achieve using even less time, that is, while looking at
+time complexity.
+
+Meet Binary Search
+
+In the scenario, binary search can be implemented by guessing the number in the middle of the whole list
+say 50 if the total number of the items in the whole list is 100 and our target number is 100.
+The person would then ask if 50 is the target number then I'll tell him that the number provided is too low.
+In this case the person will eliminate all the numbers from 1 to 50 because they are irrelevant. He will then
+pick another number and keep on eliminating untill he reaches to 100 which is the target number.
+
+Given that Linear Search requires trying out from the first number one all the way to the target number 100,
+it would take a lot of time to reach to the target number than Binary Search which works by guessing a middle
+number, then eliminating half of the numbers that are irrelevant to reaching the target number hence reaching
+the target number faster.
+"""
