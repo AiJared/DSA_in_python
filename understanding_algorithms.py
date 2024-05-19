480a0c5b57a3a17a1ae257ddbe71d246a15b6cb6
Kerriea-star <ojared511@gmail.com>
2024-05-19 10:10:52 +0300

    understanding algorithms

---
+++ understanding_algorithms.py
@@ -0,0 +1,22 @@
+# Understaning Algorithms
+"""
+An algorithm is a set of steps taken by a program to perform a task. 
+"""
+
+#Algorithmic Thinking
+"""
+In a real life senario we can use the game of thinking about a number and
+letting someone try to predict the number we are thinking about, for example,
+the number can be between 1 and 10, both 1 and 10 included. The steps that they
+would take to get the correct number might vary from person to person because 
+some might use steps that would get them to the correct answer quicker than others.
+
+Some might start guesing the numbers from the beginning 1 and progress by adding 1 until
+they get the correct number while some might start from a number in the middle say 5 then
+determine how close they are to the correct number and adjust there thinking.
+
+The one which will take fewer steps to the get to the correct answer is the winner as it is
+the most effective strategy.
+
+That is what is called algorithmic thinking.
+"""
