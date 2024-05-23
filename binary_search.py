f0d312ff77e9dd18b6803208bc7f99bcc86c55f1
Kerriea-star <ojared511@gmail.com>
2024-05-23 11:41:56 +0300

    binary search algorithm

--- binary_search.py
+++ binary_search.py
@@ -7,12 +7,29 @@ def binary_search(list, target):
     while first <= last:
         midpoint = (first + last)//2
 
-    # evaluate whether value at midpoint is our target value
-    if list[midpoint] == target:
-        return midpoint
-    elif list[midpoint] < target:
-        first = midpoint + 1
-    else:
-        last = midpoint - 1
+        # evaluate whether value at midpoint is our target value
+        if list[midpoint] == target:
+            return midpoint
+        elif list[midpoint] < target:
+            first = midpoint + 1
+        else:
+            last = midpoint - 1
 
     return None
+
+# A function to verify the binary search algorithm above
+def verify(index):
+    if index is not None:
+        print("Target found at index: ", index)
+    else:
+        print("Target not found in list")
+
+# list of values
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+# check if our algorithm works
+result = binary_search(numbers, 12)
+verify(result)
+
+result = binary_search(numbers, 6)
+verify(result)
