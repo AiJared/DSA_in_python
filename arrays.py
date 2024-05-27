020c918121f6fe1c8218e1e930f5dfd913519bd4
Kerriea-star <ojared511@gmail.com>
2024-05-27 15:51:00 +0300

    accessing values in an array

---
+++ arrays.py
@@ -0,0 +1,10 @@
+# List in python
+new_list = [1, 2, 3]
+# accessing a value in an array using index
+result = new_list[0]
+print(result)
+
+# trying to access an index value larger than what already exists in the array 
+# would result into the array crashing and give an IndexError.
+# For Example
+print(new_list[5]) 
