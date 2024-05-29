236c165c7ba8be9de419eb601961c2e945835f45
Kerriea-star <ojared511@gmail.com>
2024-05-29 08:00:01 +0300

    linked lists in Python

--- linked_list.py
+++ linked_list.py
@@ -31,3 +31,38 @@ class LinkedList:
     """
     def __init__(self):
         self.head = None
+
+    """
+    Ideally we would try not to expose the inner workings of our data structure
+    to the code that uses it. Instead let's make this operation expression explicity
+    by defining a method.
+    """
+    def is_empty(self):
+        return self.head == None
+    """
+    All we doing here is checking to see if head is None. If it is then the return
+    gives us True which evaluates that the list is empty.
+
+    Let's add one more convinience method to calculate the size of our list. The name
+    convinience method indicates that what this method is doing is not providing any
+    additional functionality that our data structure can't handle right now but instead
+    making existing functionality easier to use.
+
+    We could calculate the size of our linked list by traversing it everytime using a loop
+    until we hit a tail node but doing that everytime is a husle.  
+    """
+    def size(self):
+        """
+        Returns the number of nodes in the list.
+        Takes O(n) time.
+        """
+        current = self.head
+        count = 0
+
+        while current:
+            count += 1
+            current = current.next_node
+        
+        return count
+    
+
