f20e045bf19552719b5f8abc0f548b175d28528f
Kerriea-star <ojared511@gmail.com>
2024-05-31 12:08:02 +0300

    insert in linked lists

--- linked_list.py
+++ linked_list.py
@@ -87,6 +87,16 @@ class LinkedList(Node):
             else:
                 current = current.next_node
         return None
+    
+    def insert(self, data, index):
+        if index == 0:
+            self.add(data)
+        
+        if index > 0:
+            new = Node(data)
+            position = index
+            current = self.head
+            
 
     def __repr__(self):
         """
