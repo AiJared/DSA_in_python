646929cd49a77ed399037fc3ffa7bb61c75c8dca
Kerriea-star <ojared511@gmail.com>
2024-06-03 13:16:01 +0300

    merge sort and linked list

--- linked_list_merge_sort.py
+++ linked_list_merge_sort.py
@@ -76,4 +76,30 @@ def merge(left, right):
         elif right_head is None:
             current.next_node = left_head
             # Call next on left to set loop condition to False
-            lef_head = left_head.next_node
+            left_head = left_head.next_node
+        else:
+            # Not at either tail node
+            # Obtain node data to perform comparison operations
+            left_data = left_head.data
+            right_data = right_head.data
+
+            # If data on left is less than right, set current to left node
+            if left_data < right_data:
+                current.next_node = left_data
+                # Move left head to next node
+                left_head = left_head.next_node
+            # If data on left is greater than right, set current to right node
+            else:
+                current.next_node = right_head
+                # Move right head to next node
+                right_head = right_head.next_node
+
+        # Move current to next node
+        current = current.next_node
+    
+    # Discard fake head and set first merged node as head
+    head = merged.head.next_node
+    merged.head = head
+
+    return merged
+
