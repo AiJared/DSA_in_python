4cae1eb3a22d052fd76178bb9574fcb0bb2c7823
Kerriea-star <ojared511@gmail.com>
2024-06-03 12:38:08 +0300

    merge sort on linked list

--- linked_list.py
+++ linked_list.py
@@ -133,6 +133,18 @@ class LinkedList(Node):
                 current = current.next_node
         return current
 
+    def node_at_index(self, index):
+        if index == 0:
+            return self.head
+        else:
+            current = self.head
+            position = 0
+
+            while position < index:
+                current = current.next_node
+                position += 1
+            return current
+
     def __repr__(self):
         """
         Returns a string representation of the list
