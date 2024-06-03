4665b1b6e3100d16293f8a359a9193cbd189ebba
Kerriea-star <ojared511@gmail.com>
2024-06-03 12:58:46 +0300

    merge sort on linked list

--- linked_list_merge_sort.py
+++ linked_list_merge_sort.py
@@ -41,3 +41,39 @@ def split(linked_list):
         mid_node.next_node = None
 
         return left_half, right_half
+    
+def merge(left, right):
+    """
+    Merges two linked lists sorting by data in the nodes
+    Returns a new merged list
+    """
+
+    # Create a new linked list that contains nodes from
+    # merging left and right
+
+    merged = LinkedList()
+
+    # Add a fake head that is discarded later
+    merged.add(0)
+
+    # set current to the head of the linked list
+    current = merged.head
+
+    # Obtain head nodes for left and right linked lists
+    left_head = left.head
+    right_head = right.head
+
+    # Iterate over left and right until we reach the tail node of either
+    while left_head or right_head:
+        # if the head node of left is None, we're past the tail
+        # add the node from right to merged linked list
+        if left_head is None:
+            current.next_node = right_head
+            # Call next on right to set loop condition to False
+            right_head = right_head.next_node
+        # If the head node of right is None, we're past the tail
+        # Add the tail node from left to merges linked list
+        elif right_head is None:
+            current.next_node = left_head
+            # Call next on left to set loop condition to False
+            lef_head = left_head.next_node
