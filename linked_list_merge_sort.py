528ccf31127b733fe061327180786921661294ea
Kerriea-star <ojared511@gmail.com>
2024-06-03 12:23:39 +0300

    linked list and merge sort

---
+++ linked_list_merge_sort.py
@@ -0,0 +1,18 @@
+from linked_list import LinkedList
+def merge_sort(linked_list):
+    """
+    sorts a linked list in ascending order
+    - Recursively divide the linked list into sublists containing a single node
+    - Repeatedly merge the sublists to produce sorted sublists until one remaims
+
+    Returns a sorted linked list
+    """
+    if linked_list.size == 1:
+        return linked_list
+    elif linked_list.head is None:
+        return linked_list
+    
+    left_half, right_half = split(linked_list)
+
+    left = merge_sort(left_half)
+    right = merge_sort(right_half)
