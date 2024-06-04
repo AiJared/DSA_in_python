173ab8e32d73e954973bd893d4e1a49c9a2e040e
Kerriea-star <ojared511@gmail.com>
2024-06-04 07:29:12 +0300

    bogo sort algorithm

--- bogo_sort.py
+++ bogo_sort.py
@@ -1,3 +1,19 @@
 import random
 import sys
 from load import load_numbers
+
+numbers = load_numbers(sys.argv[1])
+
+def is_sorted(values):
+    for index in range(len(values) - 1):
+        if values[index] > values[index + 1]:
+            return False
+        
+    return True
+
+def bogo_sort(values):
+    while not is_sorted(values):
+        random.shuffle(values)
+    return values
+
+print(bogo_sort(numbers))
