7a189fffc00b9ed7291ae57d848b14824de41729
Kerriea-star <ojared511@gmail.com>
2024-06-06 06:15:28 +0300

    quick sort

--- quicksort.py
+++ quicksort.py
@@ -15,9 +15,10 @@ def quicksort(values):
         else:
             greater_than_pivot.append(value)
     
+    print("%15s %1s %-15s" % (less_than_pivot, pivot, greater_than_pivot))
     return quicksort(less_than_pivot) + [pivot] + quicksort(greater_than_pivot)
 
-    
+print(numbers)    
 sorted_numbers = quicksort(numbers)
 print(sorted_numbers)
 
