054b7cb7d22dd6fbfde42d6f67f26887ed0634b1
Kerriea-star <ojared511@gmail.com>
2024-06-07 07:34:44 +0300

    quick sort for strings

--- quicksort_strings.py
+++ quicksort_strings.py
@@ -1,7 +1,7 @@
 import sys
-from load import load_numbers
+from load import load_strings
 
-numbers = load_numbers(sys.argv[1])
+names = load_strings(sys.argv[1])
 
 def quicksort(values):
     if len(values) <= 1:
@@ -15,10 +15,8 @@ def quicksort(values):
         else:
             greater_than_pivot.append(value)
     
-    print("%15s %1s %-15s" % (less_than_pivot, pivot, greater_than_pivot))
     return quicksort(less_than_pivot) + [pivot] + quicksort(greater_than_pivot)
 
-print(numbers)    
-sorted_numbers = quicksort(numbers)
-print(sorted_numbers)
-
+sorted_names = quicksort(names)
+for n in sorted_names:
+    print(n)
