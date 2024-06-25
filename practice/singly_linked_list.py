328ce9e9a0dec711722dfd60abf4e6d2a21b8ca7
Kerriea-star <ojared511@gmail.com>
2024-06-25 09:36:41 +0300

    operations carried out on singly linked lists

---
+++ practice/singly_linked_list.py
@@ -0,0 +1,14 @@
+class Node:
+    """
+    A node stores the data item and a reference to the next node
+    """
+    data = None
+    next_node = None
+
+    def __init__(self, data):
+        self.data = data
+
+    def __repr__(self):
+        return "<Node data: %s>" % self.data
+    
+
