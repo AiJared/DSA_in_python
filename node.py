c15721d8a0a1f475cfcb15e9f4e48277a9cbba07
Kerriea-star <ojared511@gmail.com>
2024-06-25 09:22:15 +0300

    creating a node of a linked list in Python

---
+++ node.py
@@ -0,0 +1,22 @@
+class Node:
+    """
+    A node stores data and a reference to the next node in a linked list
+    """
+    data = None
+    next_node = None
+
+    def __init__(self, data):
+        self.data = data
+
+    def __repr__(self):
+        return "<Node data: %s>" % self.data
+    
+
+
+n1 = Node(10)
+print(n1)
+n2 = Node(12)
+print(n2)
+# next node
+n1.next_node = n2
+print(n1.next_node)
