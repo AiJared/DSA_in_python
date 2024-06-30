ef4fc5d7c7b08772c7ca0e44d3d6bad31e0479f6
Kerriea-star <ojared511@gmail.com>
2024-06-30 16:51:40 +0300

    insert function for inserting new node at a given position in a singly linked list

--- practice/actual_code/singly_linked_list.py
+++ practice/actual_code/singly_linked_list.py
@@ -63,6 +63,29 @@ class SinglyLinkedList(Node):
                 current = current.next_node
         
         return None
+    
+    def insert(self, data, index):
+        """
+        Inserts new Node containing data at index position.
+        Insertion takes O(1) but finding the node at the insertion
+        point takes O(n) time. So it takes overall O(n) time.
+        """
+        if index == 0:
+            self.add(data)
+        
+        if index > 0:
+            new = Node(data)
+            position = index
+            current = self.head
+
+            while position > 1:
+                current = Node.next_node
+                position -= 1
+            prev_node = current
+            next_node = current.next_node
+
+            prev_node.next_node = new
+            new.next_node = next_node
 
 # create a singly linked list
 lst = SinglyLinkedList()
