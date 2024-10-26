c47238d46425a8024e574a5e8ffff74dddfe797f
Kerriea-star <ojared511@gmail.com>
2024-10-26 10:11:43 +0300

    method to remove elements from the top of the stack

--- stack.py
+++ stack.py
@@ -2,4 +2,14 @@ class Stack:
     def __init__(self):
         self.items = []
     
+    # Add elements ontop of the stack
+    def push(self, item):
+        self.items.append()
+    
+    # Remove elements from the top of the stack
+    def pop(self):
+        if not is_empty():
+            self.items.pop()
+        else:
+            return "Stack is empty"
     
