c72be408dff15c280f18d54a94db109b4c442c50
Kerriea-star <ojared511@gmail.com>
2024-10-28 08:29:58 +0300

    Queue data structure

--- queue_python.py
+++ queue_python.py
@@ -1,19 +1,22 @@
 # Implementing a Queue using a list
 
 class Queue:
+    # initialize an empty queue
     def __init__(self):
         self.items = []
     
+    # add elements at the end of the queue
     def enque(self, item):
         return self.items.append(item)
         
-    
+    # remove the front elements of the queue
     def deque(self):
         if not self.is_empty():
             return self.items.pop(0)
         else:
             return "Queue is empty!"
     
+    # View the front element without removing it
     def peek(self):
         if not self.is_empty():
             return self.items[0]
