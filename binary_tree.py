a7882d27c2e992b3609e47299b12332856da1cd1
Kerriea-star <ojared511@gmail.com>
2024-10-31 10:32:14 +0300

    Initializing the nodes and the value in a binary tree

---
+++ binary_tree.py
@@ -0,0 +1,7 @@
+class Node:
+    # constructor method initializing left and right nodes and value
+    def __init__(self, left, right, key):
+        self.left = None
+        self.right = None
+        self.value = key
+
