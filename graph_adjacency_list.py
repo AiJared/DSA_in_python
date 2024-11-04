85e2adbb9beafe813fa45b91c25574f8a8f76e3a
Kerriea-star <ojared511@gmail.com>
2024-11-04 09:52:17 +0300

    method to add node to the graph

--- graph_adjacency_list.py
+++ graph_adjacency_list.py
@@ -3,3 +3,10 @@ class Graph:
     def __init__(self):
         self.graph = {}
     
+    # Method to add node to a graph
+    def add_node(self, u, v):
+        if u in self.graph:
+            self.graph[u].append(v)
+        else:
+            self.graph[u] = self.graph[v]
+    
