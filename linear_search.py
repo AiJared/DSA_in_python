553ebbd566687305f1d104dc5fc6ddf0f5882964
Kerriea-star <ojared511@gmail.com>
2024-11-10 09:57:45 +0300

    method to verify the result of the linear search method

--- linear_search.py
+++ linear_search.py
@@ -37,10 +37,19 @@ class LinearSearch:
     def linear_search(self, target):
         """
         Search for the value in the list starting from the first index value.
-        If the value is in the list return its index value.
+        If the value is in the list return its index value. If not return None.
         """
         for i in range(0, len(self.lst)):
             if self.lst[i] == target:
                 return i
         return None
     
+    # Method to verify the results of the linear search method above
+    def verify(self, index):
+        """
+        If index is available print it. If it is not available print the value is not found.
+        """
+        if index is not None:
+            print("Target found at index: ", index)
+        else:
+            print("Target not found!")
